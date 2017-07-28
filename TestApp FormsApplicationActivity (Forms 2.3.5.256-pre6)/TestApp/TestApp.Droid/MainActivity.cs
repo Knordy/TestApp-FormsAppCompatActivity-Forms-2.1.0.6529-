e4e76bce8390960837582432660e8053fed413c2
Knordy <jordy.hoolwerf@gmail.com>
2017-07-28 10:53:07 +0200

    Add files via upload

---
+++ TestApp FormsApplicationActivity (Forms 2.3.5.256-pre6)/TestApp/TestApp.Droid/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+
+namespace TestApp.Droid
+{
+   [Activity(Label = "TestApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]//, Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+   public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity //FormsAppCompatActivity
+   {
+      protected override void OnCreate(Bundle bundle)
+      {
+         base.OnCreate(bundle);
+
+         Xamarin.Forms.Forms.Init(this, bundle);
+         LoadApplication(new App());
+      }
+   }
+}
+
